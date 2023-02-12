ffb3f3f37379e0a62387fb44efb3051d4f958295
Zarko Colic <zarkocolicBizz00@gmail.com>
2023-02-12 22:41:12 +0100

    dodatCardiCardItem za zakazivanje termina

---
+++ src/components/CardItem.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Button } from './Button';
+import { useState } from 'react';
+
+function CardItem({name, src, star, checked, type, makeAnAppointment, star1, star2, id, text}) {
+  return (
+    <>
+      <li className='cards__item'>
+        <div className='cards__item__link'>
+          <figure className='cards__item__pic-wrap' data-category={type=='clients' ? '“' : name}>
+            <img
+              className='cards__item__img'
+              src={src}
+            />
+          </figure>
+          <div className='cards__item__info'>
+            <p className='cards__item__text'>{text}</p>
+            {type == 'clients' ? <h5 className='cards__item__title'>{name}</h5> : <></> }
+            {type == 'clients' ? <></> : 
+              <div className='star-container'>
+                <ul>
+                  <li>                   
+                    {checked == false ? <i class="far fa-star" onClick={()=>{star1(id)}}></i> :
+                     <i class="fas fa-star" onClick={()=>star2(id)}></i>}                   
+                  </li>
+                  <li><p className='star-number'>{star}</p></li>
+                  <li>
+                    <Button 
+                      className='btns' 
+                      buttonStyle='btn--primary' 
+                      buttonSize='btn--medium' 
+                      link={'/services'} 
+                      id = {id}
+                      onClick={makeAnAppointment}
+                    >
+                      MAKE AN APPOINTMENT
+                    </Button>
+                  </li>
+                </ul>
+                
+                
+              </div>
+
+             }
+            
+          </div>
+        </div>
+      </li>
+    </>
+  );
+}
+
+export default CardItem;
---
+++ src/components/Cards.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './Cards.css';
+import CardItem from './CardItem';
+
+
+function Cards({data1, data2, title, type, makeAnAppointment, star1, star2}) {
+  return (
+    <div className='cards'>
+      <h1>{title}</h1>
+      <div className='cards__container'>
+        <div className='cards__wrapper'>
+          <ul className='cards__items'>
+           {
+             data1.map(item => (<CardItem key={item.id} id={item.id} text={item.text}
+              name={item.name} src={item.src} checked={item.checked} star={item.star} type={type} 
+              makeAnAppointment={makeAnAppointment}  star1={star1} star2={star2} />))
+           }
+          </ul>
+          {
+            data2 != null ? 
+            <ul className='cards__items'>
+              {
+                data2.map(item => (<CardItem key={item.id} id={item.id} text={item.text}
+                   name={item.name} src={item.src} checked={item.checked} star={item.star} type={type} 
+                   makeAnAppointment={makeAnAppointment} star1={star1} star2={star2} />))
+              }
+           </ul> : <></>
+          }
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Cards;
